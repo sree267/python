b39c99b09b09bd4acf1803e1026004f14d077f4b
sree267 <sreelekshmis1411@gmail.com>
2025-10-10 10:38:36 +0530

    Add files via upload

---
+++ lopp.py
@@ -0,0 +1,4 @@
+for i in range (2,10,2):
+    if(i==6):break
+    print(i)
+    print("End of the program")
---
+++ positive.py
@@ -0,0 +1,7 @@
+n=int(input("enter the number:"))
+if n==0:
+    print("ZERO");
+if n<0:
+    print("-ve number")
+if n>0:
+    print("+ve number")
